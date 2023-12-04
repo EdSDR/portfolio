9b189205bb82e162d1cc63e49b7c499a07c81c0e
Ed Castro <ed.sdr@outlook.com>
2023-12-03 23:09:34 -0300

    feat: blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
+import HeaderContainer from "~/components/HeaderContainer";
+
 export default function BlogPage() {
   return (
-    <div className="flex justify-center">
-      <div className="container flex max-w-6xl flex-col gap-36 ">
-        <div className="flex-col justify-between gap-3 rounded-2xl border border-white border-opacity-40 bg-white bg-opacity-30 p-12 align-middle text-blue-950 transition duration-500 hover:bg-opacity-40">
-          <h1>Blog</h1>
-          <p>
-            <b>W.I.P</b>, but there will be some cool stuff here soon
-          </p>
+    <HeaderContainer
+      title="Blog"
+      description="My throughts, ideas and reflections."
+    >
+      <div className="space-y-5">
+        <div className="flex flex-col items-center justify-center gap-6 rounded-2xl border border-white border-opacity-40 bg-white bg-opacity-30 p-12 text-blue-950 transition duration-500 hover:bg-opacity-40">
+          <div className="flex flex-col items-center">
+            <h2 className="text-2xl font-bold">Well...</h2>
+            <p> Looks like I haven’t written anything yet.</p>
+          </div>
         </div>
       </div>
-    </div>
+    </HeaderContainer>
   );
 }
